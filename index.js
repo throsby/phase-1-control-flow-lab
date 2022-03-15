49d2d5c4ce4611b6af0e212b517b732d3fe68caa
Throsby Wells <throsbywells@gmail.com>
2022-03-15 14:10:17 -0400

    commit tree 48275a57b892e35600b9c7f1c96a1b636229897d

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  return distance > 199 
+    ? (distance < 2501 
+      ? ("I will gladly take your thirty bucks.") 
+      : ("No can do.") ) 
+    : ("This one is on me!");
+};
+
+function ternaryCheckCity(location){
+return location === "NYC" ? "Ok, sounds good." : "No go.";
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {  
+    case 'generous':
+        return "Thank you so much.";
+      case 'not as generous':
+        return "Thank you.";
+      default:
+        return "Bye.";
+    }
 }
